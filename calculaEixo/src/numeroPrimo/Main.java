ce9aca4c3df8d2dad4578bea3ebf4e712100dfa9
Júlio toneti <tonetijulio@gmail.com>
2020-08-09 23:57:05 -0300

    tentando mandar pro github

---
+++ calculaEixo/src/numeroPrimo/Main.java
@@ -0,0 +1,40 @@
+package numeroPrimo;
+import java.util.Scanner;
+import java.util.Locale;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		double x = sc.nextDouble();
+		double y = sc.nextDouble();
+		
+		if (x == 0.0 && y == 0.0) {
+			System.out.println("Origem");
+		}
+		else if (x == 0.0) {
+			System.out.println("Eixo Y");
+		}
+		else if (y == 0.0) {
+			System.out.println("Eixo X");
+		}
+		else if (x > 0.0 && y > 0.0) {
+			System.out.println("Q1");
+		}
+		else if (x < 0.0 && y > 0.0) {
+			System.out.println("Q2");
+		}
+		else if (x < 0.0 && y < 0.0) {
+			System.out.println("Q3");
+		}
+		else {
+			System.out.println("Q4");
+		}
+		
+		sc.close();
+	}
+}
